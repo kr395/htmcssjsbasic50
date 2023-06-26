7db5155ce3e78014e758870cc9e4d02dd51ab368
Gopal Kumar Das <krgopalkumar706@gmail.com>
2023-06-26 16:24:02 +0530

    Top Bar Side bar added

--- Projects/Youtube/youtube.js
+++ Projects/Youtube/youtube.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+var menuIcon = document.querySelector(".menu_icon")
+var sidebar = document.querySelector(".sidebar")
+
+
+menuIcon.onclick = () => {
+    sidebar.classList.toggle("small_sidebar");
+    
+}
