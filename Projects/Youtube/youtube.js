e87739980a0dbbd7d2e804a9fb4dffc99a6068ad
Gopal Kumar Das <75511425+kr395@users.noreply.github.com>
2023-11-21 14:54:09 +0530

    Delete Projects directory

--- Projects/Youtube/youtube.js
+++
@@ -1,8 +0,0 @@
-var menuIcon = document.querySelector(".menu_icon")
-var sidebar = document.querySelector(".sidebar")
-
-
-menuIcon.onclick = () => {
-    sidebar.classList.toggle("small_sidebar");
-    
-}
